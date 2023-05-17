94bde08ec34bb7cfc6a2754236c1044abd677b3d
yurigadelha1 <131833132+yurigadelha1@users.noreply.github.com>
2023-05-17 20:23:19 -0300

    Update App.js teste 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças aula 26-04-2023
+          DevOps Básico IT Talent - Mudanças aula 26-04-2023 teste 4
         </p>
         <a
           className="App-link"
